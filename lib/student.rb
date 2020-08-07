c07db8af5d027596025a7ff38a36c96e917c1364
Flora Petersen <floramarina@comcast.net>
2020-08-07 21:23:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   end
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-  end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
