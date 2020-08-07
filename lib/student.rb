3984aecd560aadfbf72c5f6da494187bfd779362
Flora Petersen <floramarina@comcast.net>
2020-08-07 21:36:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,7 +95,7 @@ class Student
 
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade = 
+    SELECT * FROM students WHERE grade = ?
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
