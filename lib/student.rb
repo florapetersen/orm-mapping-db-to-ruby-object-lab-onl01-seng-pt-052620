2767206c9422922b5ea72e0e84a302ff131ac83a
Flora Petersen <floramarina@comcast.net>
2020-08-07 21:31:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade = 10 LIMIT num
+    SELECT * FROM students WHERE students.grade = 10 LIMIT 1
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
