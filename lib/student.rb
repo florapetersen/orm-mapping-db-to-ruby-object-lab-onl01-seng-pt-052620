4f62ce4e5f2cfec37d021742ad5b0a42c8ff36f4
Flora Petersen <floramarina@comcast.net>
2020-08-07 21:38:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,7 +75,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE students.grade = 10 LIMIT ?
     SQL
